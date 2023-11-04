8b9ffd222429655a68ed52c32a36116467aed83f
jrbdino <117558328+jrbdino@users.noreply.github.com>
2023-11-04 14:50:03 -0500

    Updated csv values, messing around with weighting formula

--- john test.py
+++ john test.py
@@ -1,14 +1,87 @@
 from SortedPriorityQueue import SortedPriorityQueue
 
 my_queue = SortedPriorityQueue()
-my_queue.add(0, 'Homework 1')
-my_queue.add(2, 'Homework 2')
-my_queue.add(4, 'Homework 3')
-my_queue.add(-1, 'Homework 4')
-
-
 
 for item in my_queue._data:
     print(item)
 
-print(my_queue.min())
+
+import taipy as tp
+from taipy import Config, Core, Gui
+import pandas as pd
+pd.set_option('display.max_columns', None)
+
+def build_message(name):
+    return f"Hello {name}!"
+
+def submit_scenario(state):
+    state.scenario.input_name.write(state.input_name)
+    state.scenario.submit()
+    state.message = scenario.message.read()
+def submit_homework(state):
+    state.scenario.csv_node.write(state.csv_node)
+def load_csv_file(state):
+    data = pd.read_csv(state.path)
+    print(data.columns)
+
+data = pd.read_csv("Test Homework File - Sheet1.csv")
+# print(data)
+for row in data.iterrows():
+    print(row[0], row[1])
+priority = 1 / (data['Assignment_Points'] / data['Total Points in Class(Including this assignment)'] *
+                data['Assignment_Weight'] * (1 / data['Days Left Till Due']))
+
+
+data['Priority'] = priority
+print(data)
+
+input_name_data_node_cfg = Config.configure_data_node(id="input_name")
+message_data_node_cfg = Config.configure_data_node(id="message")
+
+build_msg_task_cfg = Config.configure_task("build_msg", build_message, input_name_data_node_cfg, message_data_node_cfg)
+scenario_cfg = Config.configure_scenario("scenario", task_configs=[build_msg_task_cfg])
+
+input_name = "Taipy"
+message = None
+csv_node = None
+path = None
+
+x_range = range(1, 6)
+data = {
+    "Assignment": x_range,
+    "Weight": [x*x for x in x_range]
+}
+
+columns = "Assignment;Weight", "Squared"
+
+page = """
+<|container container-styling|
+<center>
+Homework Queue 
+</center>
+|>
+
+<|{data}|table|columns={columns[0]}|show_all|>
+<|{columns}|toggle|>
+
+<|Points|button|>
+<|Date|button|>
+<|Type|button|>
+<|Weights|button|>
+
+<center>
+<|{path}|file_selector|label=Upload Homework|on_action=load_csv_file|extensions=.csv|hover_text=Load Homework|>
+</center>
+"""
+
+Core().run()
+scenario = tp.create_scenario(scenario_cfg)
+
+stylekit = {
+    "color_primary": "#03045E",
+    "color_secondary": "#0077B6",
+    "color_background_dark": "#48CAE4",
+    "color_background_light": "#ADE8F4",
+}
+
+Gui(page).run(stylekit=stylekit)
