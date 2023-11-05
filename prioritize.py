f4bfdf9958da203534df312a135b9efc2765d7b7
jrbdino <117558328+jrbdino@users.noreply.github.com>
2023-11-04 21:30:17 -0500

    fixed priority formula

--- prioritize.py
+++ prioritize.py
@@ -1,29 +1,24 @@
 import datetime
 
-import pandas
 import pandas as pd
 from SortedPriorityQueue import SortedPriorityQueue
 
 
 def prioritize(dataframe: pd.DataFrame) -> pd.DataFrame:
     my_queue = SortedPriorityQueue()
-    day_weight = 1
-    point_weight = 1
-    weight_weight = 0
+    day_weight = 0.2
+    grade_weight = 1
+    difficulty_weight = 0.2
 
     today_date = pd.Timestamp(datetime.datetime.today())
     dataframe['Due Date'] = pd.to_datetime(dataframe['Due Date'])
     print(dataframe)
 
-    # dataframe['Days Left'] = today_date
-    # print(dataframe)
-    # dataframe['Days Left'] = dataframe['Due Date'] - dataframe['Days Left']
-    # print(dataframe)
     dataframe['Days Left'] = (dataframe['Due Date'] - today_date).dt.days
     print(dataframe)
 
-    priority = day_weight * dataframe.iloc[:, 3] + point_weight * dataframe.iloc[:, 1] + weight_weight * \
-               dataframe.iloc[:, 2]
+    priority = 1 / (day_weight * dataframe.iloc[:, 5] + grade_weight * dataframe.iloc[:, 3] + difficulty_weight *
+                    dataframe.iloc[:, 4])
 
     dataframe['Priority'] = priority
     print(dataframe)
