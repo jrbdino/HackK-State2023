be633e1b31090e83de6786733035478936f095ce
red2922 <inthavongsaj@lopers.unk.edu>
2023-11-06 23:00:25 -0600

    Added Light + Dark Mode and File Download

--- GUI.py
+++ GUI.py
@@ -3,89 +3,75 @@ from taipy import Config, Core, Gui
 import pandas as pd
 from prioritize import prioritize
 
+#File Settings
+pd.set_option('display.max_columns', None)
 
-def build_message(name):
-    return f"Hello {name}!"
+#Variables
+data = {
+    "Assignment": "Please Enter a ",
+    "Weight": ["CSV File"]
+}
 
+columns = "Assignment;Weight",
+path = None
 
-def submit_scenario(state):
-    state.scenario.input_name.write(state.input_name)
-    state.scenario.submit()
-    state.message = scenario.message.read()
+dark_mode = "container-styling_dark"
+light_mode = "container-styling_light"
+mode = light_mode
 
+content = "Homework Template.xlsx"
 
-def submit_homework(state):
-    state.scenario.csv_node.write(state.csv_node)
-
-
+#Functions
 def load_csv_file(state):
     local_data = pd.read_csv(state.path)
     data["Assignment"] = local_data.iloc[:, 0]
     data['Weight'] = local_data.iloc[:, -1]
     state.data = data
-    print(state.data)
 
-
-pd.set_option('display.max_columns', None)
-
-input_name_data_node_cfg = Config.configure_data_node(id="input_name")
-message_data_node_cfg = Config.configure_data_node(id="message")
-
-build_msg_task_cfg = Config.configure_task("build_msg", build_message, input_name_data_node_cfg, message_data_node_cfg)
-scenario_cfg = Config.configure_scenario("scenario", task_configs=[build_msg_task_cfg])
-
-input_name = "Taipy"
-message = None
-csv_node = None
-path = None
-
-x_range = range(1, 6)
-data = {
-    "Assignment": ("Please Enter a "),
-    "Weight": ["CSV File"]
+#CSS Styling
+p1_kit = {
+    "color_primary": "#1B4965",
+    "color_secondary": "#BEE9E8",
+    "color_background_dark": "#102a43",
+    "color_background_light": "#ffffff",
 }
 
-columns = "Assignment;Weight",
+t1_kit = {
+    "font-family": "Consolas,monaco,monospace",
+    "border-radius": "20px",
+    "color_background_light": "#9bd4e4",
+}
 
-page = """
-<|container container-styling|
+#HTML Style Webpage
+p1 = """
+<|toggle|theme|>
+
+<|container {mode}|
 <center>
-Tai Your Life Together
+    Tai Your Life Together
 </center>
 |>
 
 <center>
-<|{data}|table|columns={columns[0]}|show_all|width=1000px|style=tablestyle|>
-<|{columns}|toggle|>
+    <|{data}|table|columns={columns[0]}|show_all|width=1000px|style=t1_kit|page_size=10|>
+    <|{columns}|toggle|>
 </center>
 
 <center>
-<|Date|button|>
-<|Points|button|>
-<|Type|button|>
-<|Weights|button|>
+    <|Date|button|>
+    <|Points|button|>
+    <|Type|button|>
+    <|Weights|button|>
 </center>
 
 <center>
-<|{path}|file_selector|label=Upload Homework|on_action=load_csv_file|extensions=.csv|hover_text=Load Homework|>
+    <|{content}|file_download|label=Download Template|name=Template|bypass_preview=False|>
+</center>
+
+
+<center>
+    <|{path}|file_selector|label=Upload Homework|on_action=load_csv_file|extensions=.csv|hover_text=Load Homework|>
 </center>
 """
 
-Core().run()
-scenario = tp.create_scenario(scenario_cfg)
-
-stylekit = {
-    "color_primary": "#1B4965",
-    "color_secondary": "#BEE9E8",
-    "color_background_dark": "#5FA8D3",
-    "color_background_light": "#62B6CB",
-}
-
-tablestyle = {
-    "font-family": "Consolas,monaco,monospace",
-    "border-radius": "20px",
-}
-
-
-
-Gui(page).run(stylekit=stylekit,title="Tai your Life Together")
+Gui(p1).run(stylekit=p1_kit, title="Tai your Life Together")
