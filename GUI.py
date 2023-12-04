5ee8dd0162d1b943c5b3a9588c1b93e1c1b6e959
red2922 <inthavongsaj@lopers.unk.edu>
2023-12-04 02:47:42 -0600

    Added Logo and Fixed Display of Stuff

--- GUI.py
+++ GUI.py
@@ -1,7 +1,6 @@
+import datetime
 import time
-
-import taipy as tp
-from taipy import Config, Core, Gui
+from taipy import Gui
 import pandas as pd
 from prioritize import prioritize as pr
 
@@ -21,24 +20,40 @@ path = None
 
 dark_mode = "container-styling_dark"
 light_mode = "container-styling_light"
+calendar = "calendar.svg"
 mode = dark_mode
+logo = "container-styling_logo"
 
 file_name = "Template"
 content = "Homework Template.xlsx"
+curr_date = datetime.datetime.now().strftime('%m/%d/%Y')
+p_type = 1
 
 #Functions
 def load_csv_file(state):
     local_data = pd.read_csv(state.path)
     start_time = time.time()
-    local_data = pr(local_data)
+    local_data = pr(local_data, p_type)
     end_time = time.time()
     global time_comp
     time_comp = end_time - start_time
-    time_comp = f"{time_comp} seconds"
+    #time_comp = f"{time_comp} seconds"
     print(time_comp)
     data["Assignment"] = local_data.iloc[:, 0]
     data["Priority"] = local_data.iloc[:, -1]
     state.data = data
+    state.time_comp = time_comp
+    state.curr_date = datetime.datetime.now().strftime('%m/%d/%Y')
+def set_grade():
+    global p_type
+    p_type = 1
+def set_date():
+    global p_type
+    p_type = 2
+
+def set_difficulty():
+    global p_type
+    p_type = 3
 
 #CSS Styling
 p1_kit = {
@@ -58,12 +73,22 @@ t1_kit = {
 
 #HTML Style Webpage
 p1 = """
+
+<|container {logo}|
+<|{calendar}|image|hover_text=Head Empty == True|width=100px|>
+|>
+<|Current Date: {curr_date}|button|>
+
 <|toggle|theme|>
 
+<|
 <|container {mode}|
 <center>
     Tai Your Life Together
 </center>
+
+
+|>
 |>
 
 <center>
@@ -71,9 +96,9 @@ p1 = """
 </center>
 
 <center>
-    <|Grade|button|hover_text=Grade|>
-    <|Date|button|hover_text=Date|>
-    <|Difficulty|button|hover_text=Difficulty|>
+    <|Grade|button|hover_text=Grade|on_action=set_grade|>
+    <|Date|button|hover_text=Date|on_action=set_date|>
+    <|Difficulty|button|hover_text=Difficulty|on_action=set_difficulty|>
 </center>
 
 <center>
@@ -83,6 +108,11 @@ p1 = """
 <center>
     <|{content}|file_download|label=Download Template|name={file_name}|bypass_preview=False|hover_text=Download Template|>
 </center>
+
+<center>
+    <|Time: {time_comp}|button|>
+</center>
+
 """
 
 Gui(p1).run(stylekit=p1_kit, title="Tai your Life Together")
